68f3691dd466143e1b18c40933d082f51a145851
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2024-06-11 09:43:05 +0530

    working

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from bs4 import BeautifulSoup
 import html5lib
 import requests
+import pandas as pd
 
 #target url
 url = input('Enter URL: ')
@@ -12,5 +13,16 @@ try:
     response = requests.get(url)
     doc=BeautifulSoup(response.text,"html5lib")
     print(doc.prettify())
+    data = []
+    for row in doc.find_all('tr'):
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        data.append([ele for ele in cols if ele])  # Get rid of empty values
+
+    # Arrange data into a pandas DataFrame
+    df = pd.DataFrame(data, columns=['ID', 'County', 'District', 'School', 'Type', 'Public/Private', 'Charter', 'Status'])
+
+    # Save DataFrame to CSV
+    df.to_csv('output.csv', index=False)
 except:
     print("Exception occurred")
